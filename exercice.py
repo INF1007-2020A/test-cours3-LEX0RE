1f35a44584fdbd312b56b9adf9745020c79277b5
LEX0RE <gelinas.alexandre.2001@gmail.com>
2020-09-11 18:27:54 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 def capitaliser_pays(nom):
-    for i in range(2):
+    for i in range(len(nom)):
         if nom[i] <= 'Z':
             nom = nom[i-1:i+1] + chr(ord(nom[i]) + 32) + nom[i+1:]
             #nom[i:i+1] =  + chr(ord(nom[i]) + 32)
