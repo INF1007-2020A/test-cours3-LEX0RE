71dcff00b2aed5be5291910925d6d55ce78755fe
LEX0RE <gelinas.alexandre.2001@gmail.com>
2020-09-11 18:44:44 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -6,7 +6,7 @@ def capitaliser_pays(nom):
             nom = nom[0:i] + chr(ord(nom[i]) + 32) + nom[i+1:]
         if i == 0 or nom[i-1] == ' ':
             if nom.find('and') != i:
-            nom = nom[0:i] + chr(ord(nom[i]) - 32) + nom[i+1:]
+                nom = nom[0:i] + chr(ord(nom[i]) - 32) + nom[i+1:]
 
     return nom
 
